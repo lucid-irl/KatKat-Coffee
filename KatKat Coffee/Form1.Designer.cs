b5627c775243cfe7b501b09548c7c653c31acb00
lucid-irl <tranhuyhoang3103@gmail.com>
2021-07-27 19:57:49 +0700

    change size

--- KatKat Coffee/Form1.Designer.cs
+++ KatKat Coffee/Form1.Designer.cs
@@ -167,9 +167,9 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(62, 37);
+            this.pictureBox1.Location = new System.Drawing.Point(51, 37);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(246, 169);
+            this.pictureBox1.Size = new System.Drawing.Size(271, 169);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
